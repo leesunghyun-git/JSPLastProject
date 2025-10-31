432fa4211fe015c1ab1d587463ba49972f567fed
sunghyun9737 <sunghyun9737@gmail.com>
2025-10-31 14:14:45 +0900

    config

--- src/main/java/com/sist/commons/CreateSessionFactory.java
+++ src/main/java/com/sist/commons/CreateSessionFactory.java
@@ -1,5 +1,30 @@
 package com.sist.commons;
+import java.util.*;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.*;
 public class CreateSessionFactory {
+	private static SqlSessionFactory ssf;
+	
+	static
+	{
+		
+		try 
+		{
+			Reader reader=Resources.getResourceAsReader("Config.xml");
+			ssf=new SqlSessionFactoryBuilder().build(reader);
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+	}
 
+	public static SqlSessionFactory getSsf() {
+		return ssf;
+	}
+	
 }
